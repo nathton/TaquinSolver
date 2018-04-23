d5d2de782e68aff0acecf9d8b057bcbb20010501
nathton <n.ton03@gmail.com>
2018-04-23 20:35:47 +0800

    change structure

--- taquinsolver/taquin/urls.py
+++ taquinsolver/taquin/urls.py
@@ -14,9 +14,6 @@ app_name = 'taquin'
 #     path('<int:question_id>/vote/', views.vote, name='vote'),
 # ]
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-    path('voteq/', views.voteq, name='voteq')
+    path('', views.index, name='index'),
+    path('compute/', views.compute, name='compute')
 ]
--- taquinsolver/taquinsolver/urls.py
+++ taquinsolver/taquinsolver/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('taquin/', include('taquin.urls')),
-    url(r'^$', include('cv.urls')),
+    path('cv', include('cv.urls')),
 ]
